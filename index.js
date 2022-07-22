7c17302fba44974f7bfe641711810bc0bab1f4d9
shoaib <mshoaib.85@gmail.com>
2022-07-22 23:56:29 +0500

    few changes

--- index.js
+++ index.js
@@ -1,34 +1,49 @@
 const fs = require("fs");
 const puppeteer = require('puppeteer')
 
-fs.readFile("./links2.json", { encoding: 'utf-8' }, async (err, data) => {
-    const browser = await puppeteer.launch({ headless: true });
+const pdf_config = {
+    dir_name: "pdf",
+    format: "A4",
+    debug: false // we don't print at all 
+}
+
+fs.readFile("./links.json", { encoding: 'utf-8' }, async (err, data) => {
+
     if (err) console.log(err)
     else {
-        let processed = 0
+        const browser = await puppeteer.launch({ headless: pdf_config.debug ? false : true });
+        let global_list = []
 
-        const link_promises = JSON.parse(data).map((x, index) => {
-            let local_resolve = null
-            const promise = new Promise(resolve => local_resolve = resolve)
-            return { promise, local_resolve, link: x, index }
+        const links_hash = JSON.parse(data)
+
+        Object.keys(links_hash).forEach(async (dir_name) => {
+            let processed = 0
+
+            const link_promises = links_hash[dir_name].map((link, index) => {
+                let local_resolve = null
+                const promise = new Promise(resolve => local_resolve = resolve)
+                return { promise, local_resolve, link: link.url, index, options: link.options }
+            })
+
+            global_list = global_list.concat(link_promises)
+
+            for (const link_promise of link_promises) {
+                const file_name = `./${pdf_config.dir_name}/${link_promise.link.replace(/\W+/g, '_')}.pdf`
+                if (fs.existsSync(`${file_name}`)) {
+                    link_promise.local_resolve(true)
+                    console.log(`${++processed}/${link_promises.length}: FOUND link = ${link_promise.link}`)
+                    continue
+                }
+
+                console.log(`processing link = ${link_promise.link}`)
+                await printPDF(browser, link_promise)
+                link_promise.promise.then(pdf => write_pdf_file({ pdf, link: link_promise.link }))
+                console.log(`${++processed}/${link_promises.length}: processed link = ${link_promise.link}`)
+            }
         })
 
 
-        for (const link_promise of link_promises) {
-            const file_name = `./pdf/${link_promise.link.replace(/\W+/g, '_')}.pdf`
-            if (fs.existsSync(`${file_name}`)) {
-                link_promise.local_resolve(true)
-                console.log(`${++processed}/${link_promises.length}: FOUND link = ${link_promise.link}`)
-                continue
-            }
-
-            console.log(`processing link = ${link_promise.link}`)
-            await printPDF(browser, link_promise)
-            link_promise.promise.then(pdf => write_pdf_file({ pdf, link: link_promise.link }))
-            console.log(`${++processed}/${link_promises.length}: processed link = ${link_promise.link}`)
-        }
-
-        Promise.all(link_promises.map(x => x.promise)).then(async () => {
+        Promise.all(global_list.map(x => x.promise)).then(async () => {
             console.log(`${link_promises.length} links are processed`);
             await browser.close()
             console.log(`browser closed`)
@@ -56,9 +71,13 @@ async function write_pdf_file({ link, pdf }) {
 async function printPDF(browser, link_promse) {
     const page = await browser.newPage();
     await page.goto(link_promse.link, { waitUntil: 'networkidle0', timeout: 180000 });
-    const pdf = await page.pdf({ format: 'A4' });
 
-    await page.close()
+    if (!pdf_config.debug) {
+        const pdf = await page.pdf({ format: link_promse.options.format ? link_promse.options.format : pdf_config.format });
+        await page.close()
+        link_promse.local_resolve(pdf)
+    } else {
 
-    link_promse.local_resolve(pdf)
+    }
+
 }
